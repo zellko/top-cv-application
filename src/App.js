7b13d1b4661ddac27ed4d0bc8e2f9ec228b91e8b
zellko <wyttenda@tutanota.com>
2022-07-23 14:46:37 +0200

    Setup React project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './styles/App.css';
+import React from 'react';
+
+console.log('test');
+
+function App() {
+	return (
+		<div className="App">
+			<div className="App-header">
+				<h2>CV React - Project setup</h2>
+			</div>
+		</div>
+	);
+}
+
+export default App;
