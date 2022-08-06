75f93f9e9ad766d8cc3c3dcba90c44a58030374d
zellko <wyttenda@tutanota.com>
2022-08-06 15:27:27 +0200

    Updates

---
+++ precache-manifest.75c7aac6af4c5c70c0d20279082378cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ba22280bc40190b76bc",
+    "url": "/top-cv-application/static/css/main.3b781937.chunk.css"
+  },
+  {
+    "revision": "3ba22280bc40190b76bc",
+    "url": "/top-cv-application/static/js/main.bd6ca5d9.chunk.js"
+  },
+  {
+    "revision": "c9a84428ba4c731d7c3f",
+    "url": "/top-cv-application/static/js/runtime~main.07f94eab.js"
+  },
+  {
+    "revision": "161cf565a3449576adfa",
+    "url": "/top-cv-application/static/js/2.6048b0c4.chunk.js"
+  },
+  {
+    "revision": "302d2a4777173492b36482a5e3041e13",
+    "url": "/top-cv-application/static/media/email.302d2a47.svg"
+  },
+  {
+    "revision": "97f63838a66d5a7ea23961fa42e92430",
+    "url": "/top-cv-application/static/media/phone.97f63838.svg"
+  },
+  {
+    "revision": "c33a1ae930613883df8a2c9093cf23b4",
+    "url": "/top-cv-application/static/media/map-marker.c33a1ae9.svg"
+  },
+  {
+    "revision": "064d471d1395e141cdcb87e6b589d29a",
+    "url": "/top-cv-application/static/media/github.064d471d.svg"
+  },
+  {
+    "revision": "9059b9f6351551a72f38ae383e454ee2",
+    "url": "/top-cv-application/index.html"
+  }
+];
